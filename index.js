5e39966dfe44fe348d4590be8bde6d9660e5a424
Harrison Quamily <quamilyh@gmail.com>
2023-03-13 18:43:11 -0400

    modify

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const name = 'YOUR-NAME';
+newHeader.innerHTML = `${name} is the champion`;
+document.body.appendChild(newHeader);
